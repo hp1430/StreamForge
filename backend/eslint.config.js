4ad67ea8f9e6aa33c86ee565b8096ba16f76515d
Himanshu Parashar <parasharhimanshu41@gmail.com>
2026-06-06 16:20:55 +0530

    added db connection

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,12 +1,24 @@
 import js from '@eslint/js';
-import globals from 'globals';
 import { defineConfig } from 'eslint/config';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
+    plugins: { js, 'simple-import-sort': simpleImportSort },
     extends: ['js/recommended'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } }
+
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.node, // Node.js globals
+        ...globals.commonjs // CommonJS globals (require, module, etc.)
+      }
+    }
   }
 ]);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import { PORT } from './configs/serverConfig.js';
+import connectDB from './configs/dbConfig.js';
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
