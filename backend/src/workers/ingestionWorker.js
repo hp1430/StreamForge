4beef035f285487641ed7b999a980dafa27ef9b5
Himanshu Parashar <parasharhimanshu41@gmail.com>
2026-06-14 19:13:56 +0530

    updated db status

--- backend/src/workers/ingestionWorker.js
+++ backend/src/workers/ingestionWorker.js
@@ -4,6 +4,7 @@ import {
 } from '@aws-sdk/client-sqs';
 import { sqsClient } from '../configs/sqsConfig.js';
 import { SQS_QUEUE_URL } from '../configs/serverConfig.js';
+import Video from '../schemas/videoSchema.js';
 
 export const startIngestionWorker = async () => {
   while (true) {
@@ -21,15 +22,24 @@ export const startIngestionWorker = async () => {
       for (const message of messages) {
         const body = JSON.parse(message.Body);
         const record = body.Records[0];
-        const key = decodeURIComponent(record.s3.object.key);
-
-        console.log(key);
+        const key = decodeURIComponent(
+          record.s3.object.key.replace(/\+/g, ' ')
+        );
 
         await sqsClient.send(
           new DeleteMessageCommand({
             QueueUrl: SQS_QUEUE_URL,
             ReceiptHandle: message.ReceiptHandle
           })
+        );
+
+        const response = await Video.findOneAndUpdate(
+          {
+            originalVideoKey: key
+          },
+          {
+            status: 'UPLOADED'
+          }
         );
       }
     } catch (error) {
